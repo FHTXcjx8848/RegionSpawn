9c8fe18924255909612ae1ac3ff6628ac63dd77d
cjx <cjx0109@qq.com>
2024-04-14 19:05:09 +0800

    first upload

---
+++ Config.cs
@@ -0,0 +1,206 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using TShockAPI;
+using JsonTool;
+using System.IO;
+using MySqlX.XDevAPI.Common;
+
+namespace RegionSpawn
+{
+    public class Setting
+    {
+        public static bool CanFindPlayerInRegion(string name)
+        {
+            foreach(var region in GetConfig().SpawnRegions)
+            {
+                var plr = region.Players.Find(plr=>plr.TSPlayer.Name == name);
+                if(plr != null)
+                    return true;
+            }
+            return false;
+        }
+        public static RPlayer GetPlayer(string name)
+        {
+            foreach (var region in GetConfig().SpawnRegions)
+            {
+                var plr = region.Players.Find(plr => plr.TSPlayer.Name == name);
+                if (plr != null)
+                    return plr;
+            }
+            return null;
+        }
+        public static SpawnRegion FindRegionByPlayer(string name)
+        {
+            foreach (var region in GetConfig().SpawnRegions)
+            {
+                var plr = region.Players.Find(plr => plr.TSPlayer.Name == name);
+                if (plr != null)
+                    return region;
+            }
+            return null;
+        }
+        public List<SpawnRegion> SpawnRegions { get; set; }
+        public static JsonRw<Setting> Config;
+        public static Setting GetConfig()
+        {
+            return Config.ConfigObj;
+        }
+        public static readonly string ConfigPath = Path.Combine(AppContext.BaseDirectory, "Conifg.json");
+        public static void InitConfig()
+        {
+            var setting = new Setting();
+            var json = new JsonRw<Setting>(ConfigPath, setting);
+            json.OnError += OnError;
+            json.OnCreating += OnCreating;
+            json.ConfigObj.SpawnRegions.ForEach(region =>
+            {
+                region.Restore();
+            });
+        }
+        static void OnError(object sender, ErrorEventArgs e)
+        {
+            TShock.Log.ConsoleError("RegionSpawn插件错误配置读取错误:" + e.ToString());
+
+        }
+        static void OnCreating(object sender, CreatingEvent e)
+        {
+            TShock.Log.ConsoleError("正在创建RigionSpawn插件配置文件...");
+        }
+    }
+    public class RPlayer
+    {
+        public TSPlayer TSPlayer { get; set; }
+        public int Kills = 0;
+        public int Deaths = 0;
+    }
+    public class Npc
+    {
+        [JsonProperty("生物ID")]
+        public int ID = 0;
+        //[JsonProperty("血量")]
+        //public int Health = 0;
+        [JsonProperty("数量")]
+        public int Count = 0;
+    }
+    public class Round
+    {
+        [JsonProperty("生成点X")]
+        public float X;
+        [JsonProperty("生成点Y")]
+        public float Y;
+        [JsonProperty("生成范围")]
+        public int Size;
+        [JsonProperty("生成速率")]
+        public int Rate;
+        [JsonProperty("怪物列表")]
+        public List<Npc> Npcs;
+        [JsonProperty("时间")]
+        public int Time = 0;
+        [JsonIgnore]
+        public int SecondCounter = 10;//倒计时
+    }
+    public class SpawnRegion
+    {
+        public bool IsInside(float x, float y)
+        {
+            return x >= RegionLeftX && x <= RegionRightX && y >= RegionLeftY && y <= RegionRightY;
+        }
+        public void Restore()
+        {
+            Start = false;
+            PreStart = false;
+            SecondCounter = StartSeconds;
+            Players = new List<RPlayer>();
+            CurrentRoundIndex = 0;
+            Rounds.ForEach(round =>
+            {
+                round.SecondCounter = round.Time;
+            });
+            //CurrentMobIndex = 0;
+        }
+        public void SpawnMob()
+        {
+            var currentround = Rounds[CurrentRoundIndex];
+            float currentx = (int)currentround.X << 4;//求出相对X点位置
+            float currenty = (int)currentround.Y << 4;//求出相对Y点位置
+            float distance = -1f;//距离
+            foreach (var plr in Players)
+            {
+                if (plr.TSPlayer.Active && !plr.TSPlayer.Dead)
+                {
+                    float currentdistance = Math.Abs(plr.TSPlayer.TPlayer.position.X + (float)(plr.TSPlayer.TPlayer.width / 2) - (currentx + (float)(5 / 2))) + Math.Abs(plr.TSPlayer.TPlayer.position.Y + (float)(plr.TSPlayer.TPlayer.height / 2) - (currenty + (float)(5 / 2)));
+                    if (distance == -1f || currentdistance < distance)
+                    {
+                        distance = currentdistance;
+                        float x;
+                        float y;
+                        double angle;//角度
+
+                        x = currentx - plr.TSPlayer.TPlayer.position.X;//求出生成点相对于玩家的位置
+                        y = currenty - plr.TSPlayer.TPlayer.position.Y;//求出生成点相对于玩家的位置
+                        if (!(x == 0 && y == 0))
+                            if (Math.Abs(x) > (100 << 4) || Math.Abs(y) > (50 << 4))
+                            {
+                                angle = Math.Atan2(y, x) * 180 / Math.PI;//求出角度
+                                x = (float)((100 << 4) * Math.Cos(angle * Math.PI / 180));//转为弧度
+                                y = (float)((100 << 4) * Math.Sin(angle * Math.PI / 180));
+
+                                currentx = x + plr.TSPlayer.TPlayer.position.X;
+                                currenty = y + plr.TSPlayer.TPlayer.position.Y;
+                            }
+                    }
+                }
+            }
+            Random random = new Random();
+            var npc = currentround.Npcs[random.Next(currentround.Npcs.Count)];
+            currentx = currentx / 16;
+            currenty = currenty / 16;
+            var tsnpc = TShock.Utils.GetNPCById(npc.ID);
+            TSPlayer.Server.SpawnNPC(tsnpc.type, tsnpc.FullName, npc.Count, (int)currentx, (int)currenty, currentround.Size, currentround.Size);
+        }
+        [JsonIgnore]
+        public List<RPlayer> Players;
+        [JsonIgnore]
+        public bool Start = false;//副本是否开启
+        [JsonIgnore]
+        public bool PreStart = false;//预开启倒计时
+        [JsonIgnore]
+        public int SecondCounter = 10;//倒计时
+        [JsonIgnore]
+        public int CurrentRoundIndex = 0;//当前round
+        //[JsonIgnore]
+        //public int CurrentMobIndex = 0;//当前Mob参数
+
+        [JsonProperty("人数到后启动时间")]
+        public int StartSeconds = 10;
+        [JsonProperty("名称")]
+        public string Name = "";
+        [JsonProperty("权限")]
+        public string Permission = "";
+        [JsonProperty("出生点X")]
+        public float SpawnX = 0;
+        [JsonProperty("出生点Y")]
+        public float SpawnY = 0;
+        [JsonProperty("左上X")]
+        public float RegionLeftX = 0;
+        [JsonProperty("左下Y")]
+        public float RegionLeftY = 0;
+        [JsonProperty("右下X")]
+        public float RegionRightX = 0;
+        [JsonProperty("右下Y")]
+        public float RegionRightY = 0;
+        [JsonProperty("最小开启人数")]
+        public int MinStartPlayer = 2;
+        [JsonProperty("重生时间")]
+        public int RespawnTime = 5;
+        [JsonProperty("完成后执行")]
+        public List<string> Commands = new List<string>();
+        [JsonProperty("死亡可复活")]
+        public bool Respawn = true;
+        [JsonProperty("进度限制")]
+        public int Progress = 0;
+        [JsonProperty("波次")]
+        public List<Round> Rounds = new List<Round>();
+    }
+}
---
+++ MainPlugin.cs
@@ -0,0 +1,370 @@
+﻿using System.Reflection;
+using Newtonsoft.Json;
+using Terraria;
+using TerrariaApi.Server;
+using TShockAPI;
+using System;
+using System.Timers;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Xna.Framework;
+using System.Linq;
+
+namespace RegionSpawn
+{
+    [ApiVersion(2, 1)]
+    public class Plugin : TerrariaPlugin
+    {
+        public Plugin(Main game) : base(game)
+        {
+        }
+        public override string Name
+        {
+            get
+            {
+                return "RegionSpawn";
+            }
+        }
+
+        public override Version Version
+        {
+            get
+            {
+                return Assembly.GetExecutingAssembly().GetName().Version;
+            }
+        }
+        public override string Author
+        {
+            get
+            {
+                return "Cjx";
+            }
+        }
+
+        public override string Description
+        {
+            get
+            {
+                return "副本模式插件";
+            }
+        }
+        public override void Initialize()
+        {
+            Setting.InitConfig();
+            ServerApi.Hooks.ServerLeave.Register(this, new HookHandler<LeaveEventArgs>(this.OnPlayerLeave));
+            ServerApi.Hooks.GameUpdate.Register(this, new HookHandler<EventArgs>(this.OnGameUpdate));
+            TShockAPI.GetDataHandlers.PlayerSpawn += OnPlayerSpawn;
+            TShockAPI.GetDataHandlers.KillMe += OnKillMe;
+            ServerApi.Hooks.NpcKilled.Register(this, OnNpcKilled);
+            Commands.ChatCommands.Add(new Command("regionspawn.use", PlayerCommand, "rs","副本","regionspawn"));
+            Update.Elapsed += OnUpdate;
+            Update.Start();
+        }
+        static readonly Timer Update = new Timer(1000);//秒时钟
+        public static List<SpawnRegion> GetRegionList()
+        {
+            return Setting.GetConfig().SpawnRegions;
+        }
+        public bool Lock = false;//计时锁
+        public void OnUpdate(object Sender, EventArgs e)
+        {
+            if (Lock) return;
+            Lock = true;
+            foreach (SpawnRegion region in GetRegionList())
+            {
+                if (region.Start)
+                {
+                    if (region.Players.Count >= region.MinStartPlayer)
+                    {
+                        var currentround = region.Rounds[region.CurrentRoundIndex];
+                        region.SecondCounter--;
+                        currentround.SecondCounter--;
+                        bool flag = false;//判断转场
+                        if (currentround.SecondCounter <= 0 && region.Rounds.Count != region.CurrentRoundIndex+1)
+                        {
+                            region.CurrentRoundIndex++;
+                            region.SecondCounter = region.Rounds[region.CurrentRoundIndex].Rate;
+                            foreach (var plr in region.Players)
+                            {
+                                plr.TSPlayer.Teleport(region.SpawnX * 16, region.SpawnY * 16);
+                                plr.TSPlayer.SendErrorMessage($"第{region.CurrentRoundIndex}波终止!");
+                                plr.TSPlayer.SendMessage($"第{region.CurrentRoundIndex+1}波开始!将持续{region.Rounds[region.CurrentRoundIndex].Time}秒!", Color.Blue);
+                                if(region.Rounds.Count != region.CurrentRoundIndex + 1)
+                                {
+                                    plr.TSPlayer.SendErrorMessage($"最后一波!");
+                                }
+                            }
+                            flag = true;
+                            //换波数
+                        }
+                        else if (currentround.SecondCounter <= 0 && region.Rounds.Count == region.CurrentRoundIndex + 1)//结束
+                        {
+                            flag = true;
+                            region.Players.Sort((max, min) => min.Kills.CompareTo(max.Kills));
+                            foreach (var plr in region.Players)
+                            {
+                                plr.TSPlayer.Teleport(Main.spawnTileX * 16, Main.spawnTileY * 16);//传送回畜生点
+                                plr.TSPlayer.SendErrorMessage($"副本[{region.Name}]结束!");
+                                plr.TSPlayer.SendInfoMessage($"你的击杀数:{plr.Kills}");
+                                if (plr.Deaths > 0)
+                                {
+                                    plr.TSPlayer.SendInfoMessage($"你的死亡数:{plr.Deaths}");
+                                }
+                                plr.TSPlayer.SendInfoMessage($"Mvp击杀玩家[{region.Players.First().TSPlayer.Name}]数量:{region.Players.First().Kills}");
+                            }
+                            foreach (var cmd in region.Commands)
+                            {
+                                //.HandleCommand(TSPlayer.Server, cmd.Replace("name",));
+                                //执行指令部分
+                            }
+                            //正常副本结束部分
+                            region.Restore();
+                        }
+                        else if(currentround.SecondCounter <= 5)//倒计时
+                        {
+                            foreach (var plr in region.Players)
+                            {
+                                plr.TSPlayer.SendErrorMessage($"[倒计时]第{region.CurrentRoundIndex+1}波还剩{currentround.SecondCounter}秒!");
+                            }
+                        }
+                        if (region.SecondCounter <= 0 && !flag)//正常召唤
+                        {
+                            region.SecondCounter = currentround.Rate;
+                            region.SpawnMob();
+                        }
+                    }
+                    else
+                    {
+                        //开始后人数不足
+                        foreach (var plr in region.Players)
+                        {
+                            plr.TSPlayer.Teleport(Main.spawnTileX * 16, Main.spawnTileY * 16);//传送回畜生点
+                            plr.TSPlayer.SendErrorMessage($"副本[{region.Name}]因人数不足而终止!");
+                        }
+                        region.Restore();
+                    }
+                }
+                else
+                {
+                    if (region.Players.Count >= region.MinStartPlayer)
+                    {
+                        if (region.PreStart)
+                        {
+                            if(region.SecondCounter >= 0)
+                            {
+                                region.SecondCounter--;//开始倒计时
+                            }
+                            else
+                            {
+                                region.Start = true;
+                                region.SecondCounter = region.Rounds[0].Rate;
+                                foreach(var plr in region.Players)
+                                {
+                                    plr.TSPlayer.Teleport(region.SpawnX * 16, region.SpawnY * 16);
+                                    plr.TSPlayer.SendInfoMessage($"副本[{region.Name}]开始!已将您传送至指定位置!");
+                                    plr.TSPlayer.SendMessage($"第1波开始!将持续{region.Rounds[0].Time}秒!", Color.Blue);
+                                }
+                                //开启副本
+                            }
+                        }
+                        else
+                        {
+                            TSPlayer.All.SendMessage($"副本[{region.Name}]将在{region.StartSeconds}后启动!", Color.Blue);
+                            region.PreStart = true;
+                            //开启副本开始倒计时
+                        }
+                    }
+                    else
+                    {
+                        if (region.PreStart)
+                        {
+                            region.PreStart = false;
+                            region.SecondCounter = region.StartSeconds;
+                            //中断倒计时并初始化
+                        }
+                    }
+                }
+            }
+            Lock = false;
+        }
+        private void OnNpcKilled(NpcKilledEventArgs args)//Npc被击杀
+        {
+            try
+            {
+                var tsplr = TShock.Players[args.npc.lastInteraction];
+                if (Setting.CanFindPlayerInRegion(tsplr.Name))
+                {
+                    foreach (var region in Setting.GetConfig().SpawnRegions)
+                    {
+                        var plr = region.Players.Find(plr => plr.TSPlayer.Name == tsplr.Name);
+                        if (plr != null)
+                        {
+                            plr.Kills++;
+                            return;
+                        }
+                    }
+                }
+            }
+            catch
+            {
+
+            }
+        }
+        private void OnKillMe(object sender, GetDataHandlers.KillMeEventArgs args)//玩家死亡
+        {
+            var tsplr = args.Player;
+            int respawntime = 0;
+            if (Setting.CanFindPlayerInRegion(tsplr.Name))
+            {
+                foreach (var region in Setting.GetConfig().SpawnRegions)
+                {
+                    tsplr.SendErrorMessage($"玩家[{tsplr.Name}]在挑战副本[{region.Name}]时被击败了");
+                    var plr = region.Players.Find(plr => plr.TSPlayer.Name == tsplr.Name);
+                    if (plr != null)
+                    {
+                        if (!region.Respawn)
+                        {
+                            tsplr.SendErrorMessage("你失败了!自动退出该副本!");
+                            region.Players.Remove(plr);
+                        }
+                        else
+                        {
+                            plr.Deaths++;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                TSPlayer.All.SendErrorMessage($"玩家[{tsplr}]突然斯了");
+            }
+            args.Player.Spawn(0, respawntime);
+            args.Handled = true;
+        }
+        private void OnPlayerSpawn(object sender, GetDataHandlers.SpawnEventArgs args)
+        {
+            var tsplr = args.Player;
+            if (Setting.CanFindPlayerInRegion(tsplr.Name))
+            {
+                if (args.SpawnContext == PlayerSpawnContext.ReviveFromDeath)
+                {
+                    var region = Setting.FindRegionByPlayer(tsplr.Name);
+                    tsplr.Teleport(region.SpawnX * 16, region.SpawnY * 16);
+                    args.Handled = true;
+                }
+            }
+        }
+        private void OnGameUpdate(EventArgs args)//判断玩家是否在副本区域
+        {
+            foreach (TSPlayer tsplr in TShock.Players)
+            {
+                foreach (SpawnRegion region in GetRegionList())
+                {
+                    var find = region.Players.Find(p => p.TSPlayer.Name == tsplr.Name);
+                    if (find != null && !region.IsInside(tsplr.X, tsplr.Y))
+                    {
+                        tsplr.Teleport(region.SpawnX * 16, region.SpawnY * 16);
+                        tsplr.SendErrorMessage("检测到你离开副本范围!已将您送回副本重生点!");
+                    }
+                    if(find == null && region.IsInside(tsplr.X, tsplr.Y))
+                    {
+                        tsplr.Teleport(Main.spawnTileX * 16, Main.spawnTileY * 16);
+                        tsplr.SendErrorMessage("不得擅自进入副本区域!");
+                    }
+                }
+            }
+        }
+        public static void PlayerCommand(CommandArgs args)//玩家指令部分
+        {
+            if(args.Parameters.Count < 1)
+            {
+                args.Player.SendInfoMessage("[i:4956]RegionSpawn副本系统[i:4956]\n-/rs(副本) list(列表),查看当前副本点列表\n-/rs(副本) <副本名称>,加入一个副本\n-/rs(副本) quit(退出),退出当前副本");
+            }
+            else
+            {
+                switch(args.Parameters[0])
+                {
+                    case "list"://副本列表指令
+                    case "列表":
+                        args.Player.SendInfoMessage($"[i:4956]RegionSpawn副本列表:[i:4956]");
+                        foreach(var region in GetRegionList())
+                        {
+                            if (args.Player.HasPermission(region.Permission))
+                            {
+                                args.Player.SendInfoMessage($"[i:149]{region.Name}");
+                            }
+                        }
+                        break;
+                    case "quit"://退出副本指令
+                    case "退出":
+                        if (Setting.CanFindPlayerInRegion(args.Player.Name)) 
+                        {
+                            var currentregion = Setting.FindRegionByPlayer(args.Player.Name);
+                            args.Player.Teleport(Main.spawnTileX * 16, Main.spawnTileY * 16);
+                            currentregion.Players.Remove(Setting.GetPlayer(args.Player.Name));
+                            args.Player.SendSuccessMessage("成功退出副本!");
+                        }
+                        else
+                        {
+                            args.Player.SendErrorMessage("你还未在一个副本当中!");
+                        }
+                        break;
+                    default://加入副本指令
+
+                        if (Setting.CanFindPlayerInRegion(args.Player.Name))
+                        {
+                            args.Player.SendErrorMessage("你已在一个副本中!");
+                        }
+                        else
+                        {
+                            string name = args.Parameters[0];
+                            var region = GetRegionList().Find(r=> r.Name == name);
+                            if(region != null && args.Player.HasPermission(region.Permission))
+                            {
+                                region.Players.Add(new RPlayer
+                                {
+                                    TSPlayer = args.Player,
+                                    Kills = 0, 
+                                    Deaths = 0
+                                });
+                                args.Player.SendSuccessMessage($"成功加入副本[{region.Name}]!");
+                            }
+                            else
+                            {
+                                args.Player.SendErrorMessage("不存在此副本!");
+                            }
+                        }
+                        break;
+                }
+            }
+        }
+        public void OnPlayerLeave(LeaveEventArgs args)//玩家退服判断
+        {
+            var tsplr = TShock.Players[args.Who];
+            try
+            {
+                foreach (SpawnRegion region in GetRegionList())
+                {
+                    var find = region.Players.Find(p => p.TSPlayer.Name == tsplr.Name);
+                    if (find != null)
+                    {
+                        region.Players.Remove(find);
+                        return;
+                    }
+                }
+            }
+            catch
+            {
+                TShock.Log.Error($"[RegionSpawn]玩家[{tsplr.Name}]退出异常!");
+            }
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+              
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
